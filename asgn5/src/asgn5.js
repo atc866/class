16adcde8dd66915a201200e75ac11cbce044a5bf
atc866 <52178565+atc866@users.noreply.github.com>
2024-06-04 05:13:13 -0700

    finish asgn5? can add more stuff if time

--- asgn5/src/asgn5.js
+++ asgn5/src/asgn5.js
@@ -3,19 +3,20 @@ import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
 import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 function main() {
 
     const canvas = document.querySelector( '#c' );
     const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
 
-    const fov = 75;
+    const fov = 45;
     const aspect = 2; // the canvas default
     const near = 0.1;
-    const far = 100;
+    const far = 500;
     const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-    camera.position.x=0;
-    camera.position.y=10;
-    camera.position.z=20;
+    camera.position.x=-35;
+    camera.position.y=3;
+    camera.position.z=15;
     class ColorGUIHelper {
       constructor(object, prop) {
         this.object = object;
@@ -72,13 +73,14 @@ function main() {
         texture.colorSpace = THREE.SRGBColorSpace;
         return texture;
       }
+    
     const materials = [
-        new THREE.MeshBasicMaterial({map: loadColorTexture('../resources/1.jpg')}),
-        new THREE.MeshBasicMaterial({map: loadColorTexture('../resources/2.jpg')}),
-        new THREE.MeshBasicMaterial({map: loadColorTexture('../resources/3.jpg')}),
-        new THREE.MeshBasicMaterial({map: loadColorTexture('../resources/4.jpg')}),
-        new THREE.MeshBasicMaterial({map: loadColorTexture('../resources/5.jpg')}),
-        new THREE.MeshBasicMaterial({map: loadColorTexture('../resources/6.jpg')}),
+        new THREE.MeshBasicMaterial({map: loadColorTexture('../resources/mariokartcube.png')}),
+        new THREE.MeshBasicMaterial({map: loadColorTexture('../resources/mariokartcube.png')}),
+        new THREE.MeshBasicMaterial({map: loadColorTexture('../resources/mariokartcube.png')}),
+        new THREE.MeshBasicMaterial({map: loadColorTexture('../resources/mariokartcube.png')}),
+        new THREE.MeshBasicMaterial({map: loadColorTexture('../resources/mariokartcube.png')}),
+        new THREE.MeshBasicMaterial({map: loadColorTexture('../resources/mariokartcube.png')}),
       ];
     //cube stuff
     const boxWidth = 1;
@@ -90,16 +92,65 @@ function main() {
 
         const cube = new THREE.Mesh(geometry, materials);
         scene.add(cube);
-       
         cube.position.x = x;
         cube.position.y=y;
         cube.position.z=z;
+        cube.scale.x=0.6
+        cube.scale.y=0.6
+        cube.scale.z=0.6
+     
+        
        
         return cube;
     }
     const cubes = [
-        makeInstance(geometry, materials,  0,1,1,1,1,1),
+        makeInstance(geometry, materials,  5,0.5,9.5),
+        makeInstance(geometry, materials,  5,0.5,10.5),
+        makeInstance(geometry, materials,  5,0.5,11.5),
+        makeInstance(geometry, materials,  -8,0.5,-12),
+        makeInstance(geometry, materials,  -8,0.5,-13),
+        makeInstance(geometry, materials,  -8,0.5,-14),
+        makeInstance(geometry, materials,  -75,2,0),
+        makeInstance(geometry, materials,  -76,2,0),
+        makeInstance(geometry, materials,  -77,2,0),
+        makeInstance(geometry, materials,  80,2,0),
+        makeInstance(geometry, materials, 81,2,0),
+        makeInstance(geometry, materials,  82,2,0),
     ];
+    
+    let height=0;
+    for (let i = 0; i < 6; i++) {
+      height+=0.6;
+      if(i%2==0){
+        makeInstance(geometry,new THREE.MeshBasicMaterial( {color: 0xffffff} ),-11,height,7);
+      }
+      else{
+        makeInstance(geometry,new THREE.MeshBasicMaterial( {color: 0x000000} ),-11,height,7);
+      }
+      
+    }
+    let length=7;
+    for (let i = 0; i < 12; i++) {
+      length+=0.6;
+      if(i%2==0){
+        makeInstance(geometry,new THREE.MeshBasicMaterial( {color: 0xffffff} ),-11,height,length);
+      }
+      else{
+        makeInstance(geometry,new THREE.MeshBasicMaterial( {color: 0x000000} ),-11,height,length);
+      }
+      
+    }
+    for (let i = 0; i < 6; i++) {
+      height-=0.6;
+      if(i%2==0){
+        makeInstance(geometry,new THREE.MeshBasicMaterial( {color: 0xffffff} ),-11,height,length);
+      }
+      else{
+        makeInstance(geometry,new THREE.MeshBasicMaterial( {color: 0x000000} ),-11,height,length);
+      }
+      
+    }
+
     
     //sphere stuff
     const geometrysphere = new THREE.SphereGeometry( 1, 16, 32 ); 
@@ -132,77 +183,343 @@ function main() {
     }
     const cones=[makeInstanceCone(geometrycone,0xff00a4,3,1,1)];
 //importing 3d model
-const mtlLoader = new MTLLoader();
-const objLoader= new OBJLoader();
-mtlLoader.load('../resources/cup_of_tea.mtl', (mtl) => {
-  mtl.preload();
-  objLoader.setMaterials(mtl);
-  objLoader.load('../resources/cup_of_tea.obj', (object) => {
-    object.position.x=5;
-    object.position.y=1;
-    object.position.z=1
-    scene.add(object);
-  });
-});
+
+const glbloader = new GLTFLoader();
+  glbloader.load( '../resources/race_track_south_loop.glb', function ( gltf ) {
+    const model=gltf.scene;
+    model.position.x = 0
+    model.position.y = 0
+    model.position.z=0
+    model.rotation.y=55;
+    model.scale.x=0.25
+    model.scale.y=0.25
+    model.scale.z=0.25
+
+    
+    scene.add(model);
+  }, 
+  function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+  
+  },undefined, function ( error ) {
+  
+    console.error( error );
+  
+  } );
+  const carglbloader = new GLTFLoader();
+  carglbloader.load( '../resources/ferrari_f40.glb', function ( gltf ) {
+    const model=gltf.scene;
+    model.scale.x=0.5;
+    model.scale.y=0.5;
+    model.scale.z=0.6;
+    model.rotation.y=Math.PI/2;
+    model.position.x=-20;
+    model.position.y=0.55;
+    model.position.z=11;
+    scene.add(model);
+  }, 
+  
+  function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+  
+  },undefined, function ( error ) {
+  
+    console.error( error );
+  
+  } );
+
+  carglbloader.load( '../resources/redshell.glb', function ( gltf ) {
+    const model=gltf.scene;
+    model.scale.x=0.0015;
+    model.scale.y=0.0015;
+    model.scale.z=0.0015;
+    model.rotation.y=Math.PI/2;
+    model.position.x=-23;
+    model.position.y=1;
+    model.position.z=13;
+    scene.add(model);
+  }, 
+  
+  function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+  
+  },undefined, function ( error ) {
+  
+    console.error( error );
+  
+  } );
+  carglbloader.load( '../resources/greenshell.glb', function ( gltf ) {
+    const model=gltf.scene;
+    model.scale.x=0.002;
+    model.scale.y=0.002;
+    model.scale.z=0.002;
+    model.rotation.y=Math.PI/2;
+    model.position.x=-27;
+    model.position.y=1;
+    model.position.z=12;
+    scene.add(model);
+  }, 
+  
+  function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+  
+  },undefined, function ( error ) {
+  
+    console.error( error );
+  
+  } );
+  carglbloader.load( '../resources/bulletbill.glb', function ( gltf ) {
+    const model=gltf.scene;
+    model.scale.x=0.001;
+    model.scale.y=0.001;
+    model.scale.z=0.001;
+    model.position.x=-15;
+    model.position.y=1;
+    model.position.z=-15;
+    scene.add(model);
+  }, 
+  
+  function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+  
+  },undefined, function ( error ) {
+  
+    console.error( error );
+  
+  } );
+
+  carglbloader.load( '../resources/mclaren.glb', function ( gltf ) {
+    const model=gltf.scene;
+    model.scale.x=0.35;
+    model.scale.y=0.35;
+    model.scale.z=0.45;
+    model.rotation.y=Math.PI/2;
+    model.position.x=-15;
+    model.position.y=0.25;
+    model.position.z=12;
+    scene.add(model);
+  }, 
+  
+  function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+  
+  },undefined, function ( error ) {
+  
+    console.error( error );
+  
+  } );
+  carglbloader.load( '../resources/sonic.glb', function ( gltf ) {
+    const model=gltf.scene;
+    model.scale.x=0.025;
+    model.scale.y=0.025;
+    model.scale.z=0.025;
+    model.rotation.y=Math.PI/2;
+    model.position.x=-20.5;
+    model.position.y=0.55;
+    model.position.z=13;
+    scene.add(model);
+  }, 
+  
+  function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+  
+  },undefined, function ( error ) {
+  
+    console.error( error );
+  
+  } );
+  carglbloader.load( '../resources/porsche.glb', function ( gltf ) {
+    const model=gltf.scene;
+    model.scale.x=0.25;
+    model.scale.y=0.25;
+    model.scale.z=0.25;
+    model.rotation.y=Math.PI/2;
+    model.position.x=-30;
+    model.position.y=1.25;
+    model.position.z=13;
+    scene.add(model);
+  }, 
+  
+  function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+  
+  },undefined, function ( error ) {
+  
+    console.error( error );
+  
+  } );
+  carglbloader.load( '../resources/revuelto_3.0tm.glb', function ( gltf ) {
+    const model=gltf.scene;
+    model.scale.x=0.5;
+    model.scale.y=0.4;
+    model.scale.z=0.6;
+    model.rotation.y=Math.PI*4/5
+    model.position.x=-17;
+    model.position.y=0.45;
+    model.position.z=10;
+    scene.add(model);
+  }, 
+  function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+  
+  },undefined, function ( error ) {
+  
+    console.error( error );
+  
+  } );
+  carglbloader.load( '../resources/batmobil_car.glb', function ( gltf ) {
+    const model=gltf.scene;
+    model.scale.x=0.08;
+    model.scale.y=0.07;
+    model.scale.z=0.1;
+    model.rotation.y=Math.PI
+    model.position.x=-30;
+    model.position.y=1.25;
+    model.position.z=10;
+    scene.add(model);
+  }, 
+  function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+  
+  },undefined, function ( error ) {
+  
+    console.error( error );
+  
+  } );
+  carglbloader.load( '../resources/milenium_falcon.glb', function ( gltf ) {
+    const model=gltf.scene;
+    model.scale.x=10;
+    model.rotation.y=Math.PI
+    model.position.x=-30;
+    model.position.y=3;
+    model.position.z=10;
+    scene.add(model);
+  }, 
+  function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+  
+  },undefined, function ( error ) {
+  
+    console.error( error );
+  
+  } );
+
+  carglbloader.load( '../resources/mario_kart.glb', function ( gltf ) {
+    const model=gltf.scene;
+    model.scale.x=0.5;
+    model.scale.y=0.5;
+    model.scale.z=0.5;
+    model.rotation.y=Math.PI/2;
+    model.position.x=-25;
+    model.position.y=1;
+    model.position.z=9;
+    scene.add(model);
+  }, 
+  function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+  
+  },undefined, function ( error ) {
+  
+    console.error( error );
+  
+  } );
+  carglbloader.load( '../resources/huracan.glb', function ( gltf ) {
+    const model=gltf.scene;
+    model.scale.x=0.3;
+    model.scale.y=0.3;
+    model.scale.z=0.4;
+    model.rotation.y=Math.PI/-2;
+    model.position.x=-27;
+    model.position.y=1.25;
+    model.position.z=10.5;
+    scene.add(model);
+  }, 
+  function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+  
+  },undefined, function ( error ) {
+  
+    console.error( error );
+  
+  } );
+  const bsglbloader = new GLTFLoader();
+  bsglbloader.load( '../resources/blue_shell_mario_kart.glb', function ( gltf ) {
+    const model=gltf.scene;
+    model.position.x=-15;
+    model.position.y=3;
+    model.position.z=12;
+    model.scale.x=0.005
+    model.scale.y=0.005
+    model.scale.z=0.005
+    scene.add(model);
+  }, 
+  function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+  
+  },undefined, function ( error ) {
+  
+    console.error( error );
+  
+  } );
+  const bpglbloader = new GLTFLoader();
+  bpglbloader.load( '../resources/peel.glb', function ( gltf ) {
+    const model=gltf.scene;
+    model.position.x=-15;
+    model.position.y=0.5;
+    model.position.z=10;
+    model.scale.x=0.001
+    model.scale.y=0.001
+    model.scale.z=0.001
+    scene.add(model);
+  }, 
+  function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+  
+  },undefined, function ( error ) {
+  
+    console.error( error );
+  
+  } );
+  
+  bpglbloader.load( '../resources/bobomb.glb', function ( gltf ) {
+    const model=gltf.scene;
+    model.position.x=-17.5;
+    model.position.y=1.75;
+    model.position.z=10;
+    scene.add(model);
+  }, 
+  function ( xhr ) {
+
+    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+  
+  },undefined, function ( error ) {
+  
+    console.error( error );
+  
+  } );
 //build house thing?
-var housething=
-[[1,1,1,1,0],
-[1,0,0,0,0],
-[1,0,0,0,0],
-[1,0,0,0,0],
-[1,0,0,0,0],
-[1,0,0,0,0],
-[1,0,0,0,0],
-[1,0,0,0,0],
-[1,0,0,0,0],
-[1,0,0,0,0],
-[1,0,0,0,0],
-[1,0,0,0,0],
-[1,0,0,0,0],
-[1,0,0,0,0],
-[1,0,0,0,0],
-[1,0,0,0,0],
-[1,0,0,0,0],
-[1,0,0,0,0],
-[1,0,0,0,0],
-[1,1,1,1,0]]
 
-var musicmaterialthing=new THREE.MeshPhongMaterial({map: loadColorTexture('../resources/music.jpg')});
-var musicboxgeom= new THREE.BoxGeometry(3,3,3);
-for(let height=0;height<5;height++){
-  for(let x=0;x<20;x++){
-    for(let y=0;y<5;y++){
-      if(housething[x][y]==1){
-        makeInstance(musicboxgeom,musicmaterialthing,(x-8)*3,height*3,(y-3)*3,);
-      }
-    }
-  }
-}
 
-//plane thing?
-  {
 
-    const planeSize = 100;
-    const loaderplane = new THREE.TextureLoader();
-    const textureplane = loaderplane.load('../resources/checker.png');
-    textureplane.wrapS = THREE.RepeatWrapping;
-    textureplane.wrapT = THREE.RepeatWrapping;
-    textureplane.magFilter = THREE.NearestFilter;
-    textureplane.colorSpace = THREE.SRGBColorSpace;
-    const repeats = planeSize / 2;
-    textureplane.repeat.set(repeats, repeats);
-    const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
-    const planeMat = new THREE.MeshPhongMaterial({
-      map: textureplane,
-      side: THREE.DoubleSide,
-    });
-    const meshplane = new THREE.Mesh(planeGeo, planeMat);
-    meshplane.rotation.x = Math.PI * -.5;
-    scene.add(meshplane);
-  };
+
   //lightingambient
-  const color = 0x231F1F;
-  const intensity = 0.5;
+  const color = 0xf5ed0f;
+  const intensity = 1;
   const light = new THREE.AmbientLight(color, intensity);
   scene.add(light);
   gui.addColor(new ColorGUIHelper(light, 'color'), 'value').name('color');
@@ -215,11 +532,11 @@ for(let height=0;height<5;height++){
     folder.add(vector3, 'z', -10, 10).onChange(onChangeFn);
     folder.open();
   }
-  const dircolor = 0xFB0404;
+  const dircolor = 0xffffff;
   const dirintensity = 3.5;
   const dirlight = new THREE.DirectionalLight(dircolor, dirintensity);
   dirlight.position.set(-7.7,6.8,2.14);
-  dirlight.target.position.set(8.28,0,1.64);
+  dirlight.target.position.set(-5,0,9.02);
   scene.add(dirlight);
   scene.add(dirlight.target);
   const dirhelper = new THREE.DirectionalLightHelper(dirlight);
@@ -235,10 +552,10 @@ for(let height=0;height<5;height++){
   makeXYZGUI(gui, dirlight.target.position, 'target', updateLight);
 
   //pointlight
-  const pointcolor=0x23F207;
-  const pointintensity=150;
+  const pointcolor=0x0000ff;
+  const pointintensity=1000;
   const pointlight=new THREE.PointLight(pointcolor, pointintensity);
-  pointlight.position.set(4.1,6.93,0);
+  pointlight.position.set(-15,3,12);
   scene.add(pointlight);
   const pointhelper = new THREE.PointLightHelper(pointlight);
   scene.add(pointhelper);
@@ -252,7 +569,7 @@ for(let height=0;height<5;height++){
     //background thing
   const backgroundloader = new THREE.TextureLoader();
   const backgroundtexture = backgroundloader.load(
-    '../resources/background.jpg',
+    '../resources/desert.jpg',
     () => {
       backgroundtexture.mapping = THREE.EquirectangularReflectionMapping;
       backgroundtexture.colorSpace = THREE.SRGBColorSpace;
